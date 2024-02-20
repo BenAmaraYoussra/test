407785f031ccd66acf0df3019956c202c9131db8
BenAmaraYoussra <benamarayousra3@gmail.com>
2024-02-20 23:02:43 +0100

    first commit

--- src/Components/TaskCard/Task.js
+++ src/Components/TaskCard/Task.js
@@ -4,21 +4,39 @@ import { useDispatch } from 'react-redux'
 import { deleteTask, doneTask } from '../../JS/Actions/actions'
 import './task.css'
 import EditTask from '../EditTask/EditTask'
+import {Row, Col} from 'react-bootstrap'
 
 const Task = ({ task }) => {
   const dispatch = useDispatch()
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <h3 className={task.isDone ? 'done' : 'undone'}>{task.caption}</h3>
+    <div 
+    //style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+>
+    <Row>
+    <Col md={6}>
+    <h3 className={task.isDone ? 'done' : 'undone'}>{task.caption}</h3>
+    </Col>
+
+    <Col md={2}>
       <EditTask task={task} />
-      <Button variant="danger" onClick={() => dispatch(deleteTask(task.id))}>
-        Delete
-      </Button>
-      <Button onClick={() => dispatch(doneTask(task.id))}>
-        {task.isDone ? 'UnDone' : 'Done'}
-      </Button>
-    </div>
+    </Col>
+    <Col md={2}>
+  {' '}
+  <Button
+  variant="danger"
+  onClick={()=> dispatch(deleteTask(task.id))}
+  >
+    Delete
+  </Button>
+  </Col>
+  <Col md={2}>
+  <Button onClick={()=>(doneTask(task.id))}>
+    {task.isDone ? 'UnDone' : 'Done'}
+  </Button>
+  </Col>
+</Row>
+</div>
   )
 }
-
 export default Task
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/css/bootstrap.min.css'
 import App from './App';
 import { Provider } from 'react-redux'
 import store from './JS/Store/store'
-
+import 'bootstrap/dist/bootstrap.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
